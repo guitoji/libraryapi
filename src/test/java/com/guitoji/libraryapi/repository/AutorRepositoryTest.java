c398c79895832d1f32890a6f2ad3c573c02d173a
guitoji <guilherme.limaal22@outlook.com>
2026-04-11 18:48:19 -0300

    Autor Repository Test part 2

--- src/test/java/com/guitoji/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/com/guitoji/libraryapi/repository/AutorRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -41,6 +43,42 @@ public class AutorRepositoryTest {
 
             autorEncontrado.setDataNascimento(LocalDate.of(1968, 6, 14));
             repository.save(autorEncontrado);
+        }
+    }
+
+    @Test
+    public void listarTest() {
+        List<Autor> lista = repository.findAll();
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    public void contagemTest() {
+        System.out.println("Contagem de autores: " + repository.count());
+    }
+
+    @Test
+    public void deletePorIdTest() {
+        var id = UUID.fromString("fa679134-7d4c-479b-9018-15691ec02d02");
+
+        Optional<Autor> possivelAutor = repository.findById(id);
+
+        if (possivelAutor.isPresent()) {
+            repository.deleteById(id);
+            System.out.println("Autor de id " + id + " deletado!");
+        }
+    }
+
+    @Test
+    public void deleteTest() {
+        var id = UUID.fromString("137d2a21-1e9f-4877-ae9a-b005ea984b48");
+
+        Optional<Autor> possivelAutor = repository.findById(id);
+
+        if ((possivelAutor.isPresent())) {
+            Autor autor = possivelAutor.get();
+            repository.delete(autor);
+            System.out.println("Autor " + possivelAutor.get().getNome() + " deletado!");
         }
     }
 }
