1ea0bb7d15f294dd12504dbe575b6cef45102247
guitoji <guilherme.limaal22@outlook.com>
2026-04-11 16:54:09 -0300

    Autor & Livro Repository

---
+++ src/main/java/com/guitoji/libraryapi/Repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.guitoji.libraryapi.Repository;
+
+import com.guitoji.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
---
+++ src/main/java/com/guitoji/libraryapi/Repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.guitoji.libraryapi.Repository;
+
+import com.guitoji.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}
