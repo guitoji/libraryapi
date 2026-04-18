0b94f3c207882dcd89768d2f9908f8860cfec67e
guitoji <guilherme.limaal22@outlook.com>
2026-04-18 15:17:16 -0300

    Adding dataCadastro and dataAtualizacao to Autor entity with JPA Auditing

--- src/main/java/com/guitoji/libraryapi/model/Autor.java
+++ src/main/java/com/guitoji/libraryapi/model/Autor.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +18,7 @@ import java.util.UUID;
 @Getter // Gera getter em tempo de compilação com LOMBOK
 @Setter // Gera setter em tempo de compilação com LOMBOK
 @ToString(exclude = "livros") // Gera toString
+@EntityListeners(AuditingEntityListener.class)
 public class Autor {
 
     // Mapeamento opcional do Column
@@ -33,4 +38,15 @@ public class Autor {
 
     @OneToMany(mappedBy = "autor") // Um autor para muitos livros
     private List<Livro> livros;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
 }
