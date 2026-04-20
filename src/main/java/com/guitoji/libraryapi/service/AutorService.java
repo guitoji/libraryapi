0a2e5155097d1adcc1cd5585c9230e1b4e1964aa
guitoji <guilherme.limaal22@outlook.com>
2026-04-20 00:24:37 -0300

    Refactoring 'filtrar' with 'pesquisaByExample' using Query by Example

--- src/main/java/com/guitoji/libraryapi/service/AutorService.java
+++ src/main/java/com/guitoji/libraryapi/service/AutorService.java
@@ -6,6 +6,8 @@ import com.guitoji.libraryapi.repository.AutorRepository;
 import com.guitoji.libraryapi.repository.LivroRepository;
 import com.guitoji.libraryapi.validator.AutorValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -46,7 +48,6 @@ public class AutorService {
     }
 
     public List<Autor> pesquisa(String nome, String nacionalidade) {
-        //Irei refatorar este algoritmo após algumas aulas, pois não é um metodo elegante
         if (nome != null && nacionalidade != null) {
             return autorRepository.findByNomeAndNacionalidade(nome, nacionalidade);
         }
@@ -60,6 +61,21 @@ public class AutorService {
         }
 
         return autorRepository.findAll();
+    }
+
+    public List<Autor> pesquisaByExample(String nome, String nacionalidade) {
+        var autor = new Autor();
+        autor.setNome(nome);
+        autor.setNacionalidade(nacionalidade);
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnorePaths("id", "dataNascimento", "dataCadastro")
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Autor> autorExample = Example.of(autor, matcher);
+        return autorRepository.findAll(autorExample);
     }
 
     public boolean possuiLivro(Autor autor) {
