632a0ab17975bb0269c2600b9eb70667f892870b
guitoji <guilherme.limaal22@outlook.com>
2026-05-03 16:01:48 -0300

    Implementing form login

--- src/main/java/com/guitoji/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/com/guitoji/libraryapi/controller/dto/AutorDTO.java
@@ -1,11 +1,9 @@
 package com.guitoji.libraryapi.controller.dto;
 
-import com.guitoji.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import org.mapstruct.MapperConfig;
 
 import java.time.LocalDate;
 import java.util.UUID;
